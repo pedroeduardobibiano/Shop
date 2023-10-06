39284da56e4ce8e0e931e01b25fcbf0753c90744
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2023-10-06 13:40:46 -0300

    last deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { Roboto } from "@next/font/google";
 import { globalStyles } from "@/styles/global";
 
-import logoImg from '../assets/logo.svg'
+import logoImg from '../assets/Logo.svg'
 import { Container, Header } from "@/styles/pages/app";
 
 import Image from "next/image";
